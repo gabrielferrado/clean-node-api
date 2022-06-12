a0ab05993bbf3d3fbeedc79d8ad7ffa1f47cc4e4
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-12 17:47:29 -0300

    chore: update config files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
