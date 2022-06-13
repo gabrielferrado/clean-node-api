2705e5e4bcff074732260a6331d2f5389d430421
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-13 14:58:07 -0300

    test: ensure AccountRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const ACCOUNT_DATA = {
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+}
+
+describe('Account Mongo Repository', function () {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add(ACCOUNT_DATA)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(ACCOUNT_DATA.name)
+    expect(account.email).toBe(ACCOUNT_DATA.email)
+    expect(account.password).toBe(ACCOUNT_DATA.password)
+  })
+})
