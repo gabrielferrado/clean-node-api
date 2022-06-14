6f7576fd0f1553e8d7c2fdaa2d3c9ac203b998d3
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-14 11:43:24 -0300

    test: add login controller

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,64 @@
+import { LoginController } from './login'
+import { badRequest } from '../../helpers/http-helpers'
+import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../../protocols/email-validator'
+
+const DEFAULT_HTTP_REQUEST = {
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+}
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+interface SutTypes {
+  sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  return {
+    sut: new LoginController(emailValidatorStub),
+    emailValidatorStub
+  }
+}
+
+describe('Login Controller', function () {
+  test('Should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    await sut.handle(DEFAULT_HTTP_REQUEST)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
