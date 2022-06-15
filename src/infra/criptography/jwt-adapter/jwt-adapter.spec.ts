a3bba3f1423bb504e39930161c715b1885c8b0d0
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-15 19:06:46 -0300

    test: ensure JwtAdapter returns a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return Promise.resolve('any_token')
+  }
+}))
+
 const VALID_ACCOUNT = {
   email: 'any_email',
   password: 'hashed_password',
@@ -14,5 +20,11 @@ describe('JWT Adapter', function () {
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt(VALID_ACCOUNT)
     expect(signSpy).toHaveBeenCalledWith(VALID_ACCOUNT , 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt(VALID_ACCOUNT)
+    expect(accessToken).toBe('any_token')
   })
 })
