2f240b5922028d9ba38220429b0060057815981b
gabrielferrado <gabrielferreira019@gmail.com>
2022-06-16 19:14:09 -0300

    test: ensure DbAddTriangle calls AddTriangleRepository with correct values

---
+++ src/data/usecases/add-triangle/db-add-triangle.spec.ts
@@ -0,0 +1,44 @@
+import { DbAddTriangle } from './db-add-triangle'
+import { AddTriangle, AddTriangleModel, AddTriangleRepository, TriangleModel } from './db-add-triangle-protocols'
+
+const VALID_SIDES = {
+  side1: 1,
+  side2: 1,
+  side3: 1
+}
+const VALID_TRIANGLE = {
+  type: 'any_type',
+  sides: [3,4,5]
+}
+
+const makeAddTriangleRepository = (): AddTriangleRepository => {
+  class AddTriangleRepositoryStub implements AddTriangleRepository {
+    async add (triangleData: AddTriangleModel): Promise<TriangleModel> {
+      return Promise.resolve({ id: 'any_id', ...VALID_TRIANGLE })
+    }
+  }
+  return new AddTriangleRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddTriangle
+  addTriangleRepositoryStub: AddTriangle
+}
+
+const makeSut = (): SutTypes => {
+  const addTriangleRepositoryStub = makeAddTriangleRepository()
+  const sut = new DbAddTriangle(addTriangleRepositoryStub)
+  return {
+    sut,
+    addTriangleRepositoryStub
+  }
+}
+
+describe('DbAddTriangle UseCase', function () {
+  test('Should call AddTriangleRepository with correct values', async () => {
+    const { sut, addTriangleRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addTriangleRepositoryStub, 'add')
+    await sut.add(VALID_SIDES)
+    expect(addSpy).toHaveBeenCalledWith(VALID_SIDES)
+  })
+})
