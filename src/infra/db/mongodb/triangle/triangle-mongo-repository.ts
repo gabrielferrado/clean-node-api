718ca06375fc62b67361917673d69677df5ad5d8
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-17 20:51:37 -0300

    feat: ensure TriangleMongoRepository load all triangles

--- src/infra/db/mongodb/triangle/triangle-mongo-repository.ts
+++ src/infra/db/mongodb/triangle/triangle-mongo-repository.ts
@@ -2,12 +2,18 @@ import { AddTriangleRepository } from '../../../../data/protocols/db/triangle/ad
 import { AddTriangleModel } from '../../../../domain/usecases/add-triangle'
 import { TriangleModel } from '../../../../domain/models/triangle'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadTriangleRepository } from '../../../../data/protocols/db/triangle/load-triangle-repository'
 
-export class TriangleMongoRepository implements AddTriangleRepository {
+export class TriangleMongoRepository implements AddTriangleRepository, LoadTriangleRepository {
   async add (triangleData: AddTriangleModel): Promise<TriangleModel> {
     const triangleCollection = await MongoHelper.getCollection('triangles')
     const result = await triangleCollection.insertOne(triangleData)
     const triangle = await triangleCollection.findOne({ _id: result.insertedId })
     return MongoHelper.map({ ...triangle })
+  }
+
+  async loadAll (): Promise<TriangleModel[]> {
+    const triangleCollection = await MongoHelper.getCollection('triangles')
+    return (await triangleCollection.find().toArray() as unknown) as TriangleModel[]
   }
 }
