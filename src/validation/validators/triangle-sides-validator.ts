d191bf16c50cb4a1b3d28f6c3e5a89abc6c532e2
gabrielferrado <gabrielferreira019@gmail.com>
2022-06-16 22:13:30 -0300

    feat: add triangle classifier

---
+++ src/validation/validators/triangle-sides-validator.ts
@@ -0,0 +1,11 @@
+import { Sides, TriangleValidator } from '../protocols/triangle-validator'
+import { InvalidParamError } from '../../presentation/errors'
+
+export class TriangleSidesValidator {
+  constructor (private readonly triangleValidator: TriangleValidator) {}
+
+  validate (input: Sides): Error {
+    const isValid = this.triangleValidator.classify(input)
+    if (!isValid) return new InvalidParamError('sides')
+  }
+}
