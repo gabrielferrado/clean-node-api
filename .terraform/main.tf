acc384ac4546e988b33c70343ee79d264b517e4c
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-19 00:20:09 -0300

    chore: update terraform action

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 2.63"
 }
 
 
 terraform {
-  backend "s3" {
-    bucket = "tfstate-450097264690"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
+  cloud {
+    organization = "gabrielferrado"
+
+    workspaces {
+      name = "gh-actions"
+    }
   }
 }
