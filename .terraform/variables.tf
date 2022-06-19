c77c7baea585bcc77bc1b73b861331773ed7fcd2
Gabriel Ferreira <gabrielferreira019@gmail.com>
2022-06-19 00:49:26 -0300

    chore: update terraform action

--- .terraform/variables.tf
+++ .terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "key_name" {
   type        = string
   description = "The name for ssh key, used for aws_launch_configuration"
+  default     = "ecs_triangles"
 }
 
 variable "cluster_name" {
   type        = string
   description = "The name of AWS ECS cluster"
+  default     = "ecs_triangles_cluster"
 }
